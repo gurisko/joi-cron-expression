b0d3d3c7a8a0c9d4942550fc81ca5d393e967643
Matus Gura <matus.gura@gmail.com>
2018-01-03 16:14:06 +0100

    Changed usage style

--- joi-cron-expression.js
+++ joi-cron-expression.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const Joi = require('joi');
 const cronParser = require('cron-parser');
 
-module.exports = Joi.extend({
-  name: 'string',
-  base: Joi.string(),
-  language: {
-    cron: 'needs to be a cron expression'
-  },
-  rules: [
-    {
-      name: 'cron',
-      validate(params, value, state, options) {
-        try {
-          cronParser.parseExpression(value);
-        } catch (err) {
-          return this.createError('string.cron', {v: value}, state, options);
+module.exports = (Joi) => {
+  return Joi.extend((Joi) => ({
+    name: 'string',
+    base: Joi.string(),
+    language: {
+      cron: 'needs to be a cron expression',
+    },
+    rules: [
+      {
+        name: 'cron',
+        validate(params, value, state, options) {
+          try {
+            cronParser.parseExpression(value);
+          } catch (err) {
+            return this.createError('string.cron', {v: value}, state, options);
+          }
+          return value;
         }
-        return value;
       }
-    }
-  ]
-});
-
+    ]
+  }));
+};
